fa895a5756c163b24582a5f98f21cf3b0ad1587b
Morgan <mblevins@mit.edu>
2024-04-03 13:37:03 -0400

    new PbTaSe2 work and Fan2002 example

--- morgan/Fan_fig12.lua
+++ morgan/Fan_fig12.lua
@@ -6,18 +6,11 @@
 S = S4.NewSimulation()
 S:SetLattice({1,0}, {0,1})
 S:SetNumG(100)
-epsxxr  = -21
-epsxxi  = 15
-epsyyr  = -17
-epsyyi  = 11
-S:AddMaterial("PbTaSe2", { 
-    {epsxxr, epsxxi}, {0, 0}, {0, 0},
-	{0, 0}, {epsxxr, epsxxi}, {0, 0},
-	{0, 0}, {0, 0}, {epsyyr, epsyyi}
-	})
+S:AddMaterial("Silicon", {12,0}) -- real and imag parts
 S:AddMaterial("Vacuum", {1,0})
+
 S:AddLayer('AirAbove', 0 , 'Vacuum')
-S:AddLayer('Slab', 0.5, 'PbTaSe2')
+S:AddLayer('Slab', 0.5, 'Silicon')
 S:SetLayerPatternCircle('Slab', 'Vacuum', {0,0}, 0.2)
 S:AddLayerCopy('AirBelow', 0, 'AirAbove')
 
@@ -27,21 +20,11 @@ S:SetExcitationPlanewave(
 	{0,0}) -- p-polarization amplitude and phase
 
 --S:UsePolarizationDecomposition()
-file = "PbTaSe2_data.txt"
-function exgest(file)
-    local f = io.open(file, 'w')
-    io.output(f)
-	io.write(tostring(data))
-    io.write("\n")
-    io.close(f)
- end
 
-for freq=0.25,0.27,0.003 do
+for freq=0.25,0.65,0.003 do
 	S:SetFrequency(freq)
 	forward,backward = S:GetPoyntingFlux('AirAbove', 0)
 	forward = S:GetPoyntingFlux('AirBelow', 0)
 	print (freq .. '\t' .. forward .. '\t' .. backward)
-    exgest(file, freq)
 	io.stdout:flush()
 end
- 
